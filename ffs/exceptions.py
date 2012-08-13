be67baac2314408b295bddba3e5e4b2ca9bfd262
David Miller <David>
2012-08-13 20:14:01 +0100

    Add ffs.exceptions

---
+++ ffs/exceptions.py
@@ -0,0 +1,10 @@
+"""
+ffs.exceptions
+
+Base and definitions for all exceptions raised by FFS
+"""
+class Error(Exception):
+    "Base Error class for FFS"
+
+class DoesNotExistError(Error):
+    "Something should have been here"
