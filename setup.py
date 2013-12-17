c966793d03e022b7bff587fbd2974cf4063eca88
David Miller <david@deadpansincerity.com>
2013-12-17 17:51:29 +0000

    Deactivate 3.2 until it works

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
